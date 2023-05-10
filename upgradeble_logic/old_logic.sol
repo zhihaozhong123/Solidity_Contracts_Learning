b1469fec9500ddeb2803d1addcf8e63ccd4e23fe
zhihaozhong123 <13798583510@163.com>
2023-05-10 10:48:28 +0800

    first commit

---
+++ upgradeble_logic/old_logic.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.1;
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+contract Logic is Initializable, OwnableUpgradeable {
+    function initialize() public initializer {
+        __Ownable_init();
+    }
+
+    mapping(string => uint256) private logic;
+
+    event logicSetted(string indexed _key, uint256 _value);
+
+    function SetLogic(string memory _key, uint256 _value) external {
+        logic[_key] = _value;
+        emit logicSetted(_key, _value);
+    }
+
+    function GetLogic(string memory _key) public view returns (uint256){
+        return logic[_key];
+    }
+
+    function GetLogic(string memory _key) public view returns (uint256){
+        return logic[_key] + 3000000;
+    }
+    
+    function GetInitializeData() public pure returns(bytes memory){
+        return abi.encodeWithSignature("initialize()");
+    }
+}
